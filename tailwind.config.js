bb48c8da01d72855e38007b3ce9f310131c90369
Benoit Lepage <workaholiks@gmail.com>
2025-08-04 16:41:20 +0200

    01

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     theme: {
 
         extend: {
+            fontSize: {
+                'h6': ['1rem', { lineHeight: '1.5' }],        // 16px
+                'h5': ['1.333rem', { lineHeight: '1.4' }],    // 21.33px
+                'h4': ['1.777rem', { lineHeight: '1.4' }],    // 28.44px
+                'h3': ['2.369rem', { lineHeight: '1.3' }],    // 37.93px
+                'h2': ['3.157rem', { lineHeight: '1.2' }],    // 50.58px
+                'h1': ['4.209rem', { lineHeight: '1.1' }],    // 67.44px
+            },
             colors: {
                 //add your own color
                 //https://tailwindcss.com/docs/customizing-colors
