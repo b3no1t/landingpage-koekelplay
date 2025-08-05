959cbd3447046bd660b99e29b7c75b7022bfbeff
Benoit Lepage <workaholiks@gmail.com>
2025-08-05 15:41:20 +0200

    MVP

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,19 @@ const { resolve } = require('path')
 export default {
     plugins: [],
     server: {
-        open: '/index.html',
+        cors
+            : {
+            // the origin you will be accessing via browser
+            origin
+                : 'https://cfitech.be/koekelplay',
+        },
+        open: 'index.html',
         port: 3116,
     },
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                another: resolve(__dirname, 'another.html')
+                main: resolve(__dirname, 'index.html')
             }
         }
     }
